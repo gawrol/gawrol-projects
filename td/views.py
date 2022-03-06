ac1178bfb83575f33fb05473bfd7017582d57818
gawrol <a.gawryolek@gmail.com>
2022-03-06 10:09:19 +0100

    Show tasks on index page

--- td/views.py
+++ td/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.views import View
+from td.models import Task
 
 # Create your views here.
 
 class IndexView(View):
     def get(self, request):
-        return render(request, 'td/index.html')
+        tasks = Task.objects.all()
+        context = {'tasks': tasks}
+        return render(request, 'td/index.html', context=context)
