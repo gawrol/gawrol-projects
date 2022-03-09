dbaec40a59e956981dc918344659de219cb55353
gawrol <a.gawryolek@gmail.com>
2022-03-09 08:48:58 +0100

    Remove unique attribute from task description

--- td/models.py
+++ td/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Task(models.Model):
-    desc = models.CharField(max_length=64, unique=True)
+    desc = models.CharField(max_length=64)
     state = models.BooleanField(default=False)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
